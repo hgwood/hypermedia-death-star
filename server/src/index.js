b811e3cc9d02a7f68a7b861e0ff73f180cb8ce4d
Hugo Wood <hx4.5@free.fr>
2015-07-14 13:55:21 +0200

    feat(rendering): added basic text/plain rendering

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,19 @@
 "use strict"
 
-const path = require("path")
-let renderLinks = undefined
-const express = require("express")
 const bodyParser = require("body-parser")
+const consolidate = require("consolidate")
+const express = require("express")
+const path = require("path")
+
 const app = express()
+app.engine("mustache", consolidate.mustache)
 app.use(bodyParser.urlencoded({extended: false}))
 app.set("views", path.join(__dirname, "node_modules"))
 
 app.get("/", function(req, res) {
   res.format({
     "text/plain": function() {
-      res.links({
-        start: "start"
-      }).send("Hello World! POST your name to /start to begin")
+      res.render("place.mustache", require("places/root"))
     },
     "text/html": function() {
       res.render("place.jade", require("places/root"))
@@ -45,5 +45,4 @@ const server = app.listen(3000, function() {
   const port = server.address().port
   host = host === "::" ? "localhost" : host
   console.log("Example app listening at http://%s:%s", host, port)
-  renderLinks = place => require("render-links")(`http://${host}:${port}`, place)
 })
